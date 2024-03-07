69e2daeca771da3ea37f2134f363efbf4050390c
Krunal Raol <krnlraol@gmail.com>
2024-03-07 12:40:21 -0500

    added supabase backend support to the app

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://wrcngamuykskyrqoxpss.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndyY25nYW11eWtza3lycW94cHNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk4MjM2MTgsImV4cCI6MjAyNTM5OTYxOH0.QdapicHQIPv7a3SAPjL0n-YkW2QFL1TEhNB71izDJvo";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
